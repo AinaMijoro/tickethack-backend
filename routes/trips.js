613feb641a62640341c6cb491be5ea3d7d18ec95
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-01-25 12:08:41 +0100

    test

--- routes/trips.js
+++ routes/trips.js
@@ -1,22 +1,25 @@
 var express = require("express");
 var router = express.Router();
 const Trip = require("../models/trips");
-
+const moment = require("moment");
 /* GET all trips */
-router.get("/", (req,res) => {
+router.get("/", (req, res) => {
     Trip.find().then((data) => {
-    res.json({ result: true, trips: data});
-})
-})
+        res.json({ result: true, trips: data });
+    });
+});
 
 /* GET trips by departure, arrival and date.*/
 router.post("/", (req, res) => {
-    Trip.find({ departure: req.body.departure, arrival: req.body.arrival, date: req.body.date }).then((data) => {
-        if (data !== null) {
-            const filterData = { date: { $gte: new Date(req.body.date) } };
-            res.json({ result: true, trips: data.departure});
+    Trip.find({
+        departure: req.body.departure,
+        arrival: req.body.arrival,
+        date: { $gte: req.body.date },
+    }).then((Data) => {
+        if (Data.length > 0) {
+            res.json({ Data });
         } else {
-            res.json({result: false, message: "No trips found."});
+            res.json({ error: "Trip not found" });
         }
     });
 });
